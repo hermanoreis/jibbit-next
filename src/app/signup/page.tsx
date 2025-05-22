06135022bae6d8e00f59b9f4fa84a677877a7703
hermanoreis <5004945+hermanoreis@users.noreply.github.com>
2025-05-22 20:52:55 -0300

    Add login and signup functionality

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,30 @@
+import { LogoIcon } from "@/components/icons/logo-icon"
+
+import { SignupForm } from "@/components/signup-form"
+
+export default function SignupPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <LogoIcon className="h-10 w-auto" />
+            <span className="text-xl font-black font-redhat">Jibbit</span>
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <SignupForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/placeholder.svg"
+          alt="Imagem"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  )
+} 
